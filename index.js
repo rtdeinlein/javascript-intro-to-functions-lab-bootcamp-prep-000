88c045e9d360745f6831c98c6ee5b9aafb10075d
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-03 16:15:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function shout(string){return string.toUpperCase()}
 function whisper(string){return string.toLowerCase()}
-function logshout(string){console.logshout string.toUpperCase()}
+function logshout(string){console.log .toUpperCase()}
