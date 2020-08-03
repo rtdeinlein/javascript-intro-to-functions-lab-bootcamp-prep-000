c41e8ad98e214d30c5d82286194eef8eae3659c4
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-03 16:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function logShout(string){console.log (string.toUpperCase())}
 function logWhisper(string){console.log (string.toLowerCase())}
 function sayHiToGrandma(string){ if ((string)===uppercase){return 'YES INDEED'}
 else if((string)===lowercase){return"I can't hear you!"}
-else if ((string)==="I love you, Grandma."){return"I love you, too."}
+else if ((string)==="I love you, Grandma."){return"I love you, too."}}
